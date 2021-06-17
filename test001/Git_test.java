013ee30f222fff180d60273acd854f18cddff2d5
liuweihuan <liuweihuan12@foxmail.com>
2021-06-18 00:04:19 +0800

    third

--- test001/Git_test.java
+++ test001/Git_test.java
@@ -1,8 +1,8 @@
 import org.w3c.dom.ls.LSOutput;
 
 public class Git_test {
-        LSOutput
-        LSOutput
-        LSOutput
-
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("1");
+    }
 }
