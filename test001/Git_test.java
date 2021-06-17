c3f13dc424dd961c5dde3dcb8007f015237b0e55
liuweihuan <liuweihuan12@foxmail.com>
2021-06-18 00:12:17 +0800

    third

--- test001/Git_test.java
+++ test001/Git_test.java
@@ -4,5 +4,6 @@ public class Git_test {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("1");
+        System.out.println("dev");
     }
 }
